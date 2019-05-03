b7e72b5045a2485023d735fe008945dfe1f8fe38
unknown <anton.anderzen@gmail.com>
2019-05-03 17:29:13 +0200

    Added the GC/API script with initial functions.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public int teamCount;
+    public int teamSize;
+    private int team1Score;
+    private int team2Score;
+    List<PlayerBehaviour> players;
+
+
+
+    public float GetHp(int unitID)
+    {
+        return players[unitID].GetHealth();
+    }
+
+    public List<PlayerBehaviour> VisiblePlayers(int unitID)
+    {
+        List<PlayerBehaviour> inRange = new List<PlayerBehaviour>();
+        float visionRange = players[unitID].visionRange;
+        foreach (PlayerBehaviour player in players)
+        {
+            if (player.GetID() != unitID)
+            {
+                if (Vector2.Distance(player.transform.position, players[unitID].transform.position) < visionRange){
+                    inRange.Add(player);
+                }
+            }
+        }
+        return inRange;
+    }
+
+    public bool FreeLineOfSight(int unitID, int enemyID) {
+        Ray2D(players[unitID].transform.position, players[enemyID].transform.position);
+        
+    }
+    
+    
+
+    // Force unit to look in direction of the specified angle, counterclockwise clamps between 0 and 360. 
+    public void LookInDir(int unitID, float angle) {
+        angle = Mathf.Clamp(angle, 0, 360);
+        players[unitID].transform.Rotate(new Vector3(0, 0, 1), angle);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -7,11 +7,32 @@ public class PlayerBehaviour : MonoBehaviour
     void Start()
     {
         health = maxHealth;
+        // gc = GetCompotent<GameController>(); do something like this.
     }
 
     public float visionRange = 10f;
     private float health = 0f;
     public float maxHealth = 100f;
+    private int id = -1;
+    GameController gc;
+
+
+    // Gets the hp of a unit.
+    public float GetHealth() {
+        return health;
+    }
+
+    // Gets the id of a unit.
+    public int GetID() {
+        return id;
+    }
+
+    // Sets the id of a unit.
+    public void SetID(int id) {
+        this.id = id;
+    }
+
+    // Applies damage.
     public void TakeDamage(float damage)
     {
         health -= damage;
@@ -20,6 +41,8 @@ public class PlayerBehaviour : MonoBehaviour
 
             Destroy(gameObject, 2f);
             Debug.Log("You Died");
+            //Should probably do something else, Increment team score etc. 
+
         }
     }
 }
